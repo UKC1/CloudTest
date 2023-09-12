52171b2420a45a70162617e8d26ce3c32e7ac7c2
itelleven <itelleven@gmail.com>
2023-09-12 13:47:53 +0900

    reservation clear

--- src/main/java/com/example/convenience_store/service/ReservationService.java
+++ src/main/java/com/example/convenience_store/service/ReservationService.java
@@ -1,9 +1,14 @@
 package com.example.convenience_store.service;
 
+import com.example.convenience_store.model.entity.Customer;
 import com.example.convenience_store.model.entity.Reservation;
 import com.example.convenience_store.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService {
@@ -11,8 +16,15 @@ public class ReservationService {
     private ReservationRepository reservationRepository;
 
     public Reservation save(Reservation reservation) {
-        int totalPrice = reservation.getProduct().getPrice() * reservation.getQuantity();
-        reservation.setPrice(totalPrice);
         return reservationRepository.save(reservation);
     }
+
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    public Optional<Reservation> getReservationById(Integer id) {
+        return reservationRepository.findById(id);
+    }
+
 }
